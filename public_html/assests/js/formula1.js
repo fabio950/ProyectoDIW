36daa056ef53b3f42b3cc900f0d971a3402c8609
fabio950 <fperez950@gmail.com>
2017-02-07 02:14:56 +0100

    Mejoras commit

--- public_html/assests/js/formula1.js
+++ public_html/assests/js/formula1.js
@@ -5,6 +5,7 @@ function F1Game(equipo, vidas, vidasMaximas) {
         this.equipo = equipo;
         this.vidas = vidas;
         this.vidasMaximas = vidasMaximas; 
+        this.aciertos = 0;
         this.reset();
     }
 }
@@ -25,11 +26,31 @@ F1Game.prototype.eliminarUnaVida = function () {
     this.vidas--;
 };
 
+F1Game.prototype.getNumAciertos = function () {
+    return this.aciertos;
+};
+
+F1Game.prototype.anyadirAcierto = function () {
+    this.aciertos++;
+};
+
 F1Game.prototype.coincidePilotoEnEquipo = function (idEquipoPiloto) {
     if (idEquipoPiloto === this.equipo) {
         return true;
     } else {
         this.eliminarUnaVida();
         return false;
+    }
+};
+
+F1Game.prototype.victoria = function () {
+    if(this.getNumAciertos()===2){
+        return 1;
+    }
+};
+
+F1Game.prototype.derrota = function () {
+    if(this.getNumVidas()===0){
+        return -1;
     }
 };
--- public_html/index.js
+++ public_html/index.js
@@ -13,12 +13,29 @@ $(document).ready(function () {
 function onDropHuecoTablero(event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
-    if(f1game.coincidePilotoEnEquipo(data)){
-        //pintar();
+    var idEquipo = document.getElementById(data).getAttribute("id-equipo");
+    
+    $("#" + data).attr("draggable","false");
+    
+    if(f1game.coincidePilotoEnEquipo(idEquipo)){
+        f1game.anyadirAcierto();
+        $("#" + data).addClass("animated flip");
+        $("#" + data).css("border", "2px solid green");
+        
+        if(f1game.victoria()){
+            modalVictoria();
+        }
     } else {
         pintar();
-        $("#" + data).css("filter", "grayscale(100%)");
+        $("#" + data).addClass("animated shake");
+        //$("#" + data).css("filter", "grayscale(100%)");
+        $("#" + data).css("border", "2px solid red");
+        if(f1game.derrota()){
+            modalDerrota();
+        }
     }
+    
+    
     //event.target.appendChild(document.getElementById(data));
 }
 
@@ -31,11 +48,15 @@ function drag(event) {
 }
 
 function pintar() {
-    $("#numeroVidas").attr("src", "assests/img/" + f1game.getNumVidas() + "vida.jpg");
+    $("#numeroVidas").attr("src", "assests/img/vidas/" + f1game.getNumVidas() + "-" + vidaMaxima + ".jpg");
 }
 
 function modalDificultad() {
-    btns = "<button id='5' data-dismiss='modal'>Fácil</button><button id='3' data-dismiss='modal'>Normal</button><button id='1' data-dismiss='modal'>Difícil</button>";
+    btns = "<div class='btn-group-vertical center-block botones-dificultad'>" + 
+            "<button id='5' type='button' class='btn btn-info' data-dismiss='modal'>Fácil</button>" + 
+            "<button id='3' type='button' class='btn btn-primary' data-dismiss='modal'>Normal</button>" + 
+            "<button id='1' type='button' class='btn btn-danger' data-dismiss='modal'>Difícil</button>" + 
+            "</div>";
     
     $("#f1ModalBody").append(btns);
     
@@ -46,7 +67,7 @@ function modalDificultad() {
         vidaMaxima = 5;
         f1game = new F1Game(equipos[numEquipo], vidaMaxima);
         
-        $("#numeroVidas").attr("src", "assests/img/vidas/" + vidaMaxima + "-" + f1game.getNumVidas() + ".jpg");
+        $("#numeroVidas").attr("src", "assests/img/vidas/" + f1game.getNumVidas() + "-" + vidaMaxima + ".jpg");
     });
     
     $("#3").click(function(){
@@ -64,9 +85,49 @@ function modalDificultad() {
     });
 }
 
+function modalVictoria(){
+    $("#f1ModalBody").children().remove();
+    
+    $("#f1Modal").modal("show");
+    
+    imgVictoria = "<img class='img-responsive center-block animated rubberBand' src='assests/img/trofeo.jpg'>";
+    btnReintentar = "<button id='asd' class='center-block btn btn-primary'>Jugar de nuevo</button>";
+    
+    $("#f1ModalBody").append(imgVictoria);
+    $("#f1ModalFooter").append(btnReintentar);
+    
+    $("#asd").click(function (){
+        location.reload();
+    });
+}
+
+function modalDerrota(){
+    $("#f1ModalBody").children().remove();
+    
+    $("#f1Modal").modal("show");
+    
+    imgVictoria = "<img class='img-responsive center-block animated zoomIn' src='assests/img/game-over.jpg'>";
+    btnReintentar = "<button id='asd' class='center-block btn btn-danger'>Reintentar</button>";
+    
+    $("#f1ModalBody").append(imgVictoria);
+    $("#f1ModalFooter").append(btnReintentar);
+    
+    $("#asd").click(function (){
+        location.reload();
+    });
+}
+
 
 
 // Retorna un entero aleatorio entre min (incluido) y max (excluido)
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function onMouseOverAnimacionPiloto(event) {
+    $("#" + event.target.id).css("transform", "scale(1.3, 1.3)");
+}
+
+function onMouseOutAnimacionPiloto(event) {
+    $("#" + event.target.id).css("transform", "scale(1, 1)");
 }
